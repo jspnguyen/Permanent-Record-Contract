1e3f77ae560b77da415210e105c512d120a1c46c
Joseph <theantisine@gmail.com>
2024-02-17 11:37:17 -0800

    Fixed import issues

--- contracts/mintContract.sol
+++ contracts/mintContract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ImageNFT is ERC721URIStorage, ReentrancyGuard, Ownable {
